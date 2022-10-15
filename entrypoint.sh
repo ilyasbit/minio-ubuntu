84ae68e2d8144e5054ae3e02270f74cd177ce8dd
Ilyas Fauzal Akbar <66202601+ilyasbit@users.noreply.github.com>
2022-10-16 03:43:54 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # wrapper for docker entrypoint that takes into account the PORT env var
 
-#exec minio server --console-address ":10000" --address ":443" /data
+exec minio server --console-address ":10000" --address ":443" /data
 #while true; do echo "Service Started" && sleep 600; done
 #exec minio --help
-exec tail -f /dev/null
+#exec tail -f /dev/null
